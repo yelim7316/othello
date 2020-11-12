d24e4cffac035b4e24a43212a4c1143b742f428a
yelim7316 <70632863+yelim7316@users.noreply.github.com>
2020-11-12 14:44:26 +0900

    03

--- main.c
+++ main.c
@@ -1,57 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define WHITE 0
-#define BLACK 1
-#define EMPTY 2
+#define ROW 6
+#define COLUMN 6
 
-#define WHITE_MARK " X "
-#define BLACK_MARK " O "
-#define EMPTY_MARK "  "
+#define TRUE 1
+#define FALSE 1
+
+int now_ROW;
+int now_COLUMN;
+
+void initial_board(char board[ROW][COLUMN])
+{
+	int i,j;
+	for (i=0; i<ROW; i++)
+	{
+		for (j=0; j<COLUMN; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+	
+	board[2][2] = 'W';
+	board[2][3] = 'B';
+	board[3][2] = 'B';
+	board[3][3] = 'W';
+}
 
 
-const char *row_names = "012345";
-const char *column_names = "012345";
-
-const char board [6][6] = {0};
-
-void print_board()
+void print_board(char board[ROW][COLUMN])
 {
 	
 	int i, j;
 	printf("  %d %d %d %d %d %d\n",0, 1, 2, 3, 4, 5, 6);
 	
-	for(i=0; i<6; i++)
+	for(i=0; i<ROW; i++)
 	{
 		printf(" -------------\n");
-		printf("%c|", row_names[i]);
+		printf("%d", i);
 		for (j=0; j<6; j++)
 		{
-			if (board[i][j] == WHITE)
-			{
-				printf("%s",WHITE_MARK);
-			} 
-			else if(board[i][j] == BLACK)
-			{
-			   printf("%s",BLACK_MARK);	
-			}
-			else
-			{
-				printf("%s",EMPTY_MARK);
-			}
-			printf("|");
+			printf("|%c", board[i][j]);
 		}
+		printf("|");
 		printf("\n");
 		
 	}
-	printf("\n");
 }
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	print_board();
+	char board[ROW][COLUMN];
+	initial_board(board);
+	print_board(board);
 	
 	
 	return 0;
