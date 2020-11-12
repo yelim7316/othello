c7cdb1d0c8ef6d2dd1e25c4d0d5329ccb6570dd4
yelim7316 <70632863+yelim7316@users.noreply.github.com>
2020-11-12 17:10:24 +0900

    04

--- main.c
+++ main.c
@@ -48,13 +48,39 @@ void print_board(char board[ROW][COLUMN])
 	}
 }
 
+void print_status(int black, int white, int counter)
+{
+	char *current_player = (counter % 2 == 0)? "Black":"White";
+	printf("STATUS - WHITE: %d, BLACK: %d\n", white, black);
+	printf("TURN %d: %s player's turn.\n", counter, current_player);
+}
+
+int count_Num(char board[ROW][COLUMN], char c)
+{
+	int i, j;
+	int count=0;
+	for (i=0; i<ROW; i++)
+	{
+		for (j=0; j< COLUMN; j++)
+		{
+			if (board[i][j] == c)
+			   count ++;
+		}
+	}
+	return count;
+}
+
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
 	char board[ROW][COLUMN];
+	int count_turn = 1; 
+	
 	initial_board(board);
 	print_board(board);
+	
 	
 	
 	return 0;
